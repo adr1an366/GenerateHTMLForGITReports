7703b619eb62a114cd042f4c0a61f93ae3e6d9de
eryalus <adriancamesellemartin@gmail.com>
2019-04-21 20:18:43 +0200

    Initial commit

---
+++ src/generatehtmlforgitreport/GenerateHTMLForGITReport.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generatehtmlforgitreport;
+
+import generatehtmlforgitreport.ProcessedReport.Commit;
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author eryalus
+ */
+public class GenerateHTMLForGITReport {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String projectName = "", originFile, destinationFile;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Origin file:");
+        originFile = scan.nextLine().trim();
+        File f = new File(originFile);
+        if (!f.exists() || f.isDirectory()) {
+            System.err.println("Seleted file \"" + originFile + "\" does not exists or it's a directory.");
+            return;
+        }
+        System.out.println("Destionation file:");
+        destinationFile = scan.nextLine().trim();
+        File destFile = new File(destinationFile);
+        if (destFile.isDirectory()) {
+            System.err.println("Seleted file \"" + originFile + "\" it's a directory.");
+            return;
+        } else if (destFile.exists()) {
+            System.out.print("Destination file already exists. Do you want to override? [y/n] ");
+            String op = scan.nextLine().trim();
+            if (op.toLowerCase().equals("n")) {
+                return;
+            }
+        }
+        System.out.println("Project name:");
+        projectName = scan.nextLine().trim();
+
+        try {
+            ProcessedReport report = new ProcessedReport(f);
+            InputStream templateStream = GenerateHTMLForGITReport.class.getResourceAsStream("./templates/template.html");
+            BufferedReader br = new BufferedReader(new InputStreamReader(templateStream));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(destFile)));
+            String l;
+            while ((l = br.readLine()) != null) {
+                if (l.contains("${authors_commits}")) {
+                    HashMap<String, Integer> numberOfCommitsByAuthor = report.getNumberOfCommits();
+                    for (String author : numberOfCommitsByAuthor.keySet()) {
+                        bw.write("<h3><label><font color=\"" + report.getColors().get(author) + "\">\u25A0</font></label> " + author + ": " + numberOfCommitsByAuthor.get(author) + "commits (" + String.format("%.2f", 100 * ((float) numberOfCommitsByAuthor.get(author)) / report.getCOMMITS().size()) + "%)<label><font color=\"" + report.getColors().get(author) + "\">\u25A0</font></label></h3>\n");
+                    }
+                } else if (l.contains("${table_body}")) {
+                    for (Commit c : report.getCOMMITS()) {
+                        String color = "white";
+                        String colorOfAuthor = report.getColors().get(c.getAuthor());
+                        if (colorOfAuthor != null) {
+                            color = colorOfAuthor;
+                        }
+                        bw.write("<tr bgcolor=\"" + color + "\"" + ">\n");
+                        bw.write("<td>" + c.getBranchTags().replace("\n", "<br>") + "</td>");
+                        bw.write("<td>" + c.getCommit_hash() + "</td>");
+                        bw.write("<td>" + c.getAuthor() + "</td>");
+                        bw.write("<td>" + c.getDate() + "</td>");
+                        bw.write("<td>" + c.getMerge() + "</td>");
+                        bw.write("<td>" + c.getText() + "</td>");
+                        bw.write("\n</tr>\n");
+                    }
+                } else {
+                    bw.write(l.replace("${project_name}", projectName).replace("${number_of_commits}", "" + report.getCOMMITS().size()) + "\n");
+                }
+            }
+            br.close();
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(GenerateHTMLForGITReport.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
---
+++ src/generatehtmlforgitreport/ProcessedReport.java
@@ -0,0 +1,185 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generatehtmlforgitreport;
+
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+/**
+ *
+ * @author eryalus
+ */
+public class ProcessedReport {
+
+    private ArrayList<Commit> COMMITS = new ArrayList<>();
+    private HashMap<String, Integer> NumberOfCommits = new HashMap<>();
+    private HashMap<String, String> Colors = new HashMap<>();
+    private static final String[] COLORS = new String[]{"#fffac8", "#fabebe", "#e6beff", "#ffd8b1", "#bfef45", "#f58231", "#e6194B", "#ffe119", "#aaffc3", "#3cb44b", "#42d4f4", "#4363d8", "#911eb4", "#f032e6", "#800000", "#9A6324", "#808000", "#469990"};
+
+    public HashMap<String, String> getColors() {
+        return Colors;
+    }
+
+    public HashMap<String, Integer> getNumberOfCommits() {
+        return NumberOfCommits;
+    }
+
+    public ArrayList<Commit> getCOMMITS() {
+        return COMMITS;
+    }
+
+    public ProcessedReport(File file) throws FileNotFoundException, IOException {
+        processFile(file);
+    }
+
+    private void processFile(File f) throws FileNotFoundException, IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+        String line;
+        int colorCount = 0;
+        boolean isText = false, first = true;
+        Commit commit = new Commit();
+        while ((line = reader.readLine()) != null) {
+            int i = 0;
+            while (i < line.length()) {
+                char c = line.charAt(i);
+                if (c == '*' || c == ' ' || c == '|' || c == '\\' || c == '/') {
+                    i++;
+                } else {
+                    break;
+                }
+            }
+            System.out.println(i + " " + line);
+            if (line.contains("commit") && ('c' == line.charAt(i))) {
+
+                isText = false;
+                if (first) {
+                    first = false;
+                } else {
+                    COMMITS.add(commit);
+                    commit = new Commit();
+                }
+                commit.setCommit_hash(line.split("commit")[1].trim());
+            } else if (line.contains("Author:") && ('A' == line.charAt(i))) {
+                commit.setAuthor(line.split("Author:")[1].trim().replace("<", "[").replace(">", "]"));
+                Integer commitCount = NumberOfCommits.get(commit.getAuthor());
+                if (commitCount != null) {
+                    commitCount++;
+                } else {
+                    commitCount = 1;
+                }
+                NumberOfCommits.put(commit.getAuthor(), commitCount);
+                String colorOfAuthor = Colors.get(commit.getAuthor());
+                if (colorOfAuthor == null) {
+                    if (colorCount < COLORS.length) {
+                        colorOfAuthor = COLORS[colorCount++];
+                    } else {
+                        colorOfAuthor = "#ffffff";
+                    }
+                    Colors.put(commit.getAuthor(), colorOfAuthor);
+                }
+            } else if (line.contains("Merge:") && ('M' == line.charAt(i))) {
+                commit.setMerge(line.split("Merge:")[1].trim());
+            } else if (line.contains("Date:") && ('D' == line.charAt(i))) {
+                commit.setDate(line.split("Date:")[1].trim());
+                isText = false;
+            } else {
+                if (!isText) {
+                    isText = true;
+                } else {
+                    commit.appendText(line.replace("|", "").trim());
+                }
+            }
+            commit.appendBranchTags(line.substring(0, i));
+        }
+        if (!first) {
+            COMMITS.add(commit);
+        }
+        reader.close();
+    }
+
+    public class Commit {
+
+        private String author, commit_hash, date, text = "", merge = "", branchTags = "";
+
+        public String getBranchTags() {
+            return branchTags;
+        }
+
+        public void setBranchTags(String branchTags) {
+            this.branchTags = branchTags;
+        }
+
+        public void appendBranchTags(String txt) {
+            if (branchTags.equals("")) {
+                this.branchTags = txt;
+            } else {
+                this.branchTags += "\n" + txt;
+            }
+        }
+
+        public String getMerge() {
+            return merge;
+        }
+
+        public void setMerge(String merge) {
+            this.merge = merge;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String getCommit_hash() {
+            return commit_hash;
+        }
+
+        public void setCommit_hash(String commit_hash) {
+            this.commit_hash = commit_hash;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public void appendText(String txt) {
+            if (text.equals("")) {
+                this.text = txt;
+            } else {
+                this.text += "\n" + txt;
+            }
+        }
+
+        @Override
+        public String toString() {
+            String s = "commit: " + commit_hash + "\nAuthor: " + author + "\nDate: " + date + "\n" + text;
+            return s;
+        }
+    }
+}
